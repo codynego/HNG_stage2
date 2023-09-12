29b677e0283697c88508cc336993bf83ac4917da
codynego <emonenaabednego@gmail.com>
2023-09-12 10:17:31 -0700

    quick fix

--- api/app/tests.py
+++ api/app/tests.py
@@ -1,3 +1,37 @@
+# myapp/tests.py
 from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
+from .models import Person
+from .serializers import PersonSerializer
 
-# Create your tests here.
+class PersonViewSetTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.person_data = {
+            'name': 'John Doe',
+        }
+        self.person = Person.objects.create(**self.person_data)
+
+    def test_list_persons(self):
+        response = self.client.get('/api/')  # Replace with your actual API URL
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_retrieve_person(self):
+        response = self.client.get(f'/api/{self.person.id}/')  # Replace with your actual API URL
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_person(self):
+        response = self.client.post('/api/', self.person_data, format='json')  # Replace with your actual API URL
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_update_person(self):
+        updated_data = {
+            'name': 'Updated Name',
+        }
+        response = self.client.put(f'/api/{self.person.id}/', updated_data, format='json')  # Replace with your actual API URL
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_delete_person(self):
+        response = self.client.delete(f'/api/{self.person.id}/')  # Replace with your actual API URL
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
--- api/app/views.py
+++ api/app/views.py
@@ -1,8 +1,19 @@
 from rest_framework import viewsets
 from .models import Person
 from .serializers import PersonSerializer
+from rest_framework.generics import get_object_or_404
 
 class PersonViewSet(viewsets.ModelViewSet):
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
-    lookup_field = ['name', 'id']
+    lookup_field = 'name'  # Set the default lookup field to 'name'
+
+    def get_object(self):
+        lookup_value = self.kwargs.get(self.lookup_field)
+        queryset = self.filter_queryset(self.get_queryset())
+
+        # Allow both 'id' and 'name' lookups
+        if lookup_value.isdigit():  # Check if the lookup value is numeric (ID)
+            return get_object_or_404(queryset, id=lookup_value)
+        else:  # Assume 'name' lookup
+            return get_object_or_404(queryset, name=lookup_value)
